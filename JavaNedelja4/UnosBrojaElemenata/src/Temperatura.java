2efe274a8e6897df2b040a89f1f730101c4451a0
Pavle Vesovic <119006421+r1spekt1@users.noreply.github.com>
2025-10-19 23:03:56 +0200

    Add files via upload

---
+++ JavaNedelja4/UnosBrojaElemenata/src/Temperatura.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Temperatura {
+
+	class EvidencijaTemperature{
+		private double[] temperature = new double[5];
+		
+		public EvidencijaTemperature(int brojDana) {
+			temperature = new double[brojDana];
+		}
+		
+	}
+	
+}
